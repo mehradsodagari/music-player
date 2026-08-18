381d83cf95c12320b4e17a9453fd5d3c39715e4d
mehrad <mehradvs4@gmail.com>
2026-08-18 10:06:20 +0330

    Implement volume control, playlist, and active song sync

--- script.js
+++ script.js
@@ -25,6 +25,10 @@ const audio = document.getElementById("audio");
 const duration = document.getElementById("duration");
 const currentTime = document.getElementById("current-time");
 const innerProgressBar = document.querySelector(".progress-bar-inner");
+const volume = document.getElementById("volume");
+const playlist = document.getElementById("playlist-list");
+volume.value = 50;
+audio.volume = 0.5;
 function showCurrentSong() {
   const cover = document.getElementById("cover");
   const title = document.getElementById("title");
@@ -34,6 +38,13 @@ function showCurrentSong() {
   cover.setAttribute("alt", `${currentSong.title}-${currentSong.artist}`);
   title.textContent = currentSong.title;
   artist.textContent = currentSong.artist;
+  active(playlist.querySelectorAll("li")[currentIndex]);
+}
+function active(item) {
+  playlist
+    .querySelectorAll("li")
+    .forEach((li) => li.classList.remove("active"));
+  item.classList.add("active");
 }
 function loadSong(newIndex) {
   currentIndex = newIndex;
@@ -50,6 +61,22 @@ function convertTime(seconds) {
     seconds = `0${seconds}`;
   }
   return `${minutes}:${seconds}`;
+}
+function renderPlaylist() {
+  songsList.forEach((song, index) => {
+    let songInfo = document.createElement("li");
+    let artistTitle = document.createElement("p");
+    artistTitle.textContent = `${song.title} - ${song.artist}`;
+    songInfo.addEventListener("click", (event) => {
+      loadSong(index);
+      audio.play();
+      isPlaying = true;
+      document.getElementById("status-icon").classList.remove("fa-circle-play");
+      document.getElementById("status-icon").classList.add("fa-circle-pause");
+    });
+    songInfo.appendChild(artistTitle);
+    playlist.appendChild(songInfo);
+  });
 }
 document.getElementById("play").addEventListener("click", () => {
   const playBtnIcon = document.getElementById("status-icon");
@@ -82,4 +109,32 @@ audio.addEventListener("timeupdate", () => {
   let progressPercent = (audio.currentTime / audio.duration) * 100;
   innerProgressBar.style.width = `${progressPercent}%`;
 });
+document.getElementById("progress-bar").addEventListener("click", (event) => {
+  let progressBar = event.currentTarget;
+  let totalWidth = progressBar.clientWidth;
+  let clickPosition = event.offsetX;
+  audio.currentTime = (clickPosition / totalWidth) * audio.duration;
+});
+volume.addEventListener("input", () => {
+  audio.volume = volume.value / 100;
+  let volumeIcon = document.getElementById("volume-icon");
+  if (audio.volume === 0) {
+    volumeIcon.className = "";
+    volumeIcon.classList.add("fas");
+    volumeIcon.classList.add("fa-volume-mute");
+  } else if (audio.volume <= 0.3) {
+    volumeIcon.className = "";
+    volumeIcon.classList.add("fas");
+    volumeIcon.classList.add("fa-volume-low");
+  } else if (audio.volume <= 0.7) {
+    volumeIcon.className = "";
+    volumeIcon.classList.add("fas");
+    volumeIcon.classList.add("fa-volume-medium");
+  } else {
+    volumeIcon.className = "";
+    volumeIcon.classList.add("fas");
+    volumeIcon.classList.add("fa-volume-high");
+  }
+});
+renderPlaylist();
 loadSong(0);
